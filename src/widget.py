0c758e9d7d29283727159539d97926943fd634f5
Asertyui <nezeev.niyaz009@yandex.ru>
2025-09-01 18:31:59 +0300

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from masks import get_mask_account, get_mask_card_number
 
 
@@ -21,8 +23,11 @@ def mask_account_card(input_string: str) -> str:
         return f"{" ".join(card_type)} {get_mask_card_number(numbers)}"
 
 
-def get_date(data: str) -> str:
-    """принимает на вход строку с датой в формате
-    "2024-03-11T02:26:18.671407"
-    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")."""
-    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
+def get_date(date_str: str) -> str:
+    """функция преобразования даты формата "2018-07-11T02:26:18.671407" в формат "11.07.2018" """
+    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+
+    # Форматируем объект datetime в нужный формат "ДД.ММ.ГГГГ"
+    formatted_date = date_obj.strftime("%d.%m.%Y")
+
+    return formatted_date
